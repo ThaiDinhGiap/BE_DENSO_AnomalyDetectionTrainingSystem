6c513651615b5a106ba830f272305179771425c6
giap.thaidinh <giaptdhe186094@fpt.edu.vn>
2026-01-08 22:50:24 +0700

    init project

---
+++ src/main/java/com/denso/anomaly_training_backend/AnomalyTrainingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.denso.anomaly_training_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalyTrainingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomalyTrainingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denso/anomaly_training_backend/AnomalyTrainingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denso.anomaly_training_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnomalyTrainingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
